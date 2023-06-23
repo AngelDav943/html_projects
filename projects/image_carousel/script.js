1a61989536feacf64f2c30316c20ace4dd23b772
Angel <angel92.addcs@gmail.com>
2023-06-23 12:19:18 -0500

    carousel update

--- projects/image_carousel/script.js
+++ projects/image_carousel/script.js
@@ -2,7 +2,11 @@ const carousel = document.querySelector("#carousel")
 const items = document.querySelector("#carousel").querySelectorAll(".item")
 
 var current = 0
-function update(newcurrent) {
-    if ((current+newcurrent) >= 0 && (current+newcurrent) <= (items.length/2)-1) current += newcurrent
-    carousel.scrollLeft = current * (carousel.scrollWidth / (items.length/2))
+function update(value) {
+    //if ((current+newcurrent) >= 0 && (current+newcurrent) < (items.length)-1) current += newcurrent
+    //carousel.scrollLeft = current * (carousel.scrollWidth / (items.length))
+
+    var newcurrent = current + value
+    if (newcurrent >= 0 && newcurrent < items.length) current = newcurrent
+    carousel.scrollLeft = items[current].offsetLeft
 }
