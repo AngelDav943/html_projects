c9f9447314bfadecafb1cde8cfb8db1959382477
Angel <angel92.addcs@gmail.com>
2023-06-23 12:04:37 -0500

    image_carousel update

--- projects/image_carousel/script.js
+++ projects/image_carousel/script.js
@@ -3,6 +3,6 @@ const items = document.querySelector("#carousel").querySelectorAll(".item")
 
 var current = 0
 function update(newcurrent) {
-    if (current >= 0 && current < (items.length/2)-1) current += newcurrent
+    if ((current+newcurrent) >= 0 && (current+newcurrent) <= (items.length/2)-1) current += newcurrent
     carousel.scrollLeft = current * (carousel.scrollWidth / (items.length/2))
 }
