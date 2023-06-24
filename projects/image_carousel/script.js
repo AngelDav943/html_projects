226f821a5c8a537596bc586fd96a8f81431f776b
AngelDav943 <angel91.addcs@gmail.com>
2023-06-23 19:34:12 -0500

    tasks

--- projects/image_carousel/script.js
+++ projects/image_carousel/script.js
@@ -3,10 +3,15 @@ const items = document.querySelector("#carousel").querySelectorAll(".item")
 
 var current = 0
 function update(value) {
-    //if ((current+newcurrent) >= 0 && (current+newcurrent) < (items.length)-1) current += newcurrent
+    if (current+value >= 0 && current+value < items.length) current += value
     //carousel.scrollLeft = current * (carousel.scrollWidth / (items.length))
 
-    var newcurrent = current + value
-    if (newcurrent >= 0 && newcurrent < items.length) current = newcurrent
-    carousel.scrollLeft = items[current].offsetLeft
+    /*var newcurrent = current + value
+    if (carousel.scrollLeft > 0 && carousel.scrollLeft < carousel.scrollWidth-1) current = newcurrent
+    carousel.scrollLeft = items[current].offsetLeft*/
+
+    //var number = carousel.scrollLeft + ((items[current].clientWidth) * value)
+    var number = items[current].offsetLeft
+    console.log(number)
+    carousel.scrollLeft = parseInt(Math.min(Math.max(number, 0), carousel.scrollWidth))
 }
