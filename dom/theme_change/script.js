fd316d026be1919da30997c08d6d68288a2a467f
AngelDav943 <angel91.addcs@gmail.com>
2023-07-14 19:25:56 -0500

    -

--- dom/theme_change/script.js
+++ dom/theme_change/script.js
@@ -10,10 +10,10 @@ function updat() {
     links[0].innerHTML = "html_projects github"
     title.innerText = "Wow"
 
-    const newbtn = document.createElement("button")
+    /*const newbtn = document.createElement("button")
     newbtn.innerText = "wow"
     newbtn.onclick = updat
-    cuadro.append(newbtn)
+    cuadro.append(newbtn)*/
 
     cuadro.classList.toggle("selected")
 }
