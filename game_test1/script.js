51aeff2864f8de5bf0ab718a76e1ac3364993dc1
Angel <angel92.addcs@gmail.com>
2023-07-23 22:44:44 -0500

    script update

--- game_test1/script.js
+++ game_test1/script.js
@@ -2,22 +2,32 @@ const line = document.getElementById("line")
 const target = document.getElementById("target")
 
 const pointDisplay = document.getElementById("points")
-var delay = 400
+var delay = 400;
 var points = 0;
+
+function getTargetWidth() {
+    let adjusted = (delay+100) 
+    var result = Math.max(2,Math.min(adjusted / 100, 6));
+
+    
+    console.log(result)
+    return result;
+}
 
 function generate() {
     let number = Math.floor(Math.random() * 100)
     let random = ""
 
+    let width = getTargetWidth()
+
     if (number <= 30) {
-        random = `calc(${number}% + 4em)`
+        random = `calc(${number}% + ${width}em)`
     } else if (number >= 70) {
-        random = `calc(${number}% - 4em)`
+        random = `calc(${number}% - ${width}em)`
     } else {
         random = `${number}%`
     }
-
-    console.log(random)
+    
     target.style.left = random
 }
 
@@ -28,27 +38,29 @@ function trigger() {
     var linepos = line.getClientRects()[0].x;
     var animateInfo = {duration: 1000}
 
+    
     if (linepos >= targetpos && linepos <= (targetpos + targetwidth)) {
         points += 10
-
+        
         pointDisplay.animate([
             { backgroundColor: "greenyellow"},
             { backgroundColor: "#292f3d"}
         ], animateInfo)
-
+        
         delay = Math.max(delay - 100, 200)
-        generate()
     } else {
-
+        
         pointDisplay.animate([
             { backgroundColor: "red"},
             { backgroundColor: "#292f3d"}
         ], animateInfo)
-
+        
         delay = Math.min(delay + 50, 700)
         points--;
     }
-
+    generate()
+    target.style.width = getTargetWidth() + "em"
+    
     pointDisplay.innerText = points
 }
 
